e7a73579e195bf9952c90d4dd1f9d451a86db1db
Viken-zip <viken.zip@gmail.com>
2023-02-03 10:07:11 +0200

    db connection changes for testing

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$user = 'root';
-$db = 'accounts';
-$pass = '123';
+$user = 'user_name';
+$db = 'db_name';
+$pass = 'user_password';
 
 $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
