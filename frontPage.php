76e78926a315931c9b9b10a3f7cbc3515574d91d
Viken-zip <viken.zip@gmail.com>
2023-02-03 10:03:38 +0200

    front indents

--- frontPage.php
+++ frontPage.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
     session_start();
-$uid = (isset($_SESSION['uid'])) ? $_SESSION['uid'] : '';
+    $uid = (isset($_SESSION['uid'])) ? $_SESSION['uid'] : '';
 ?>
 <html lang="en">
 <head>
